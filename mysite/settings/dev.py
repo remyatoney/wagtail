a301f694049c10d9d330b48ffe1dca5199f6b026
remyatoney <remya.toney@whiterabbit.group>
2021-09-13 09:27:15 +0530

    first commit

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-w&el8h65pedkjsn0bbyrof_*n3*4w)n+!^_-+6m#+w&(+n#&0t'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
