4f24bbbdf6df627104e5acaebe8c53ad063716c9
remyatoney <remya.toney@whiterabbit.group>
2021-09-13 11:17:42 +0530

    Adding a banner and banner fields

--- home/models.py
+++ home/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 from wagtail.core.models import Page
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel
+from wagtail.core.fields import RichTextField
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 class HomePage(Page):
     """Home page model."""
@@ -10,9 +12,27 @@ class HomePage(Page):
     max_count = 1 # Specifies that only 1 home page is allowed
 
     banner_title = models.CharField(max_length=100, blank=False, null=True) # Creates a column called banner_title in homepage table
+    banner_subtitle = RichTextField(features=["bold", "italic"]) # Richtext field allows this column to be customized to have bold or italic font
+    banner_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+"
+    )
+    banner_cta = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+"
+    )
 
     content_panels = Page.content_panels + [
-        FieldPanel("banner_title")
+        FieldPanel("banner_title"),
+        FieldPanel("banner_subtitle"),
+        ImageChooserPanel("banner_image"),
+        PageChooserPanel("banner_cta"),
     ] # Makes banner title field editable in the admin panel
 
     class Meta:
