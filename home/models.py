ad376ce3b40c2734ef08a5481adf0a8a04e6844a
remyatoney <remya.toney@whiterabbit.group>
2021-09-13 10:02:23 +0530

    Getting started with your first home page

--- home/models.py
+++ home/models.py
@@ -1,7 +1,21 @@
 from django.db import models
 
 from wagtail.core.models import Page
-
+from wagtail.admin.edit_handlers import FieldPanel
 
 class HomePage(Page):
-    pass
+    """Home page model."""
+
+    templates = "templates/home/home_page.html" # Specified the template for the homepage
+    max_count = 1 # Specifies that only 1 home page is allowed
+
+    banner_title = models.CharField(max_length=100, blank=False, null=True) # Creates a column called banner_title in homepage table
+
+    content_panels = Page.content_panels + [
+        FieldPanel("banner_title")
+    ] # Makes banner title field editable in the admin panel
+
+    class Meta:
+
+        verbose_name = "Home Page" # Calls the page Home Page (same as default)
+        verbose_name_plural = "Home Pages"
