df0200d4ada0bd2a904500a79a16590f143ddeba
remyatoney <remya.toney@whiterabbit.group>
2021-09-13 21:04:58 +0530

    How to Add a Basic StreamField to your Wagtail CMS Page

--- flex/models.py
+++ flex/models.py
@@ -3,20 +3,31 @@
 from django.db import models
 
 from wagtail.core.models import Page
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.core.fields import StreamField
+
+from streams import blocks
 
 class FlexPage(Page):
-    """Flexible page class"""
+    """Flexible page class."""
 
-    # @todo add streamfields
-    # content = StreamField()
+    template = "flex/flex_page.html"
+
+    content = StreamField(
+        [
+            ("title_and_text", blocks.TitleAndTextBlock())
+        ],
+        null=True,
+        blank=True,
+    )
 
     subtitle = models.CharField(max_length=100, null=True, blank=True)
 
     content_panels = Page.content_panels + [
         FieldPanel("subtitle"),
+        StreamFieldPanel("content")
     ]
 
     class Meta:
-        verbose_name= "Flex Page"
+        verbose_name = "Flex Page"
         verbose_name_plural = "Flex pages"
--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -27,7 +27,8 @@ INSTALLED_APPS = [
     'home',
     'search',
     'flex',
-
+    'streams',
+    
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
     'wagtail.embeds',
